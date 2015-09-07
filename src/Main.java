3a4f1ac7a313cbe66e7e3ce2ba639d16ba60a2af
Mirim <espoir@e-mirim.hs.kr>
2015-09-07 16:54:39 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("안녕 세상아!");
+        System.out.println("IntelliJ");
+    }
+}
